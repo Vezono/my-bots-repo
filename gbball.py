5b8fd1bfbb23a1ca3651e24977a1a31e131eae7b
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:42:55 +0300

    Update gbball.py

--- gbball.py
+++ gbball.py
@@ -1,1 +1,16 @@
-print('gbball.py: App launched!')
+import telebot
+import time
+import random
+import threading
+from telebot import types
+from pymongo import MongoClient
+import traceback
+
+token = '765563788:AAFnkelmaKJkalYOAJsU0fTrexErKOnl5No'
+bot = telebot.TeleBot(token)
+bot.send_message(-1001167374930, 'Хостинг')
+
+print('gbball.py: App launched!')        
+bot.polling(none_stop=True,timeout=600)
+
+
