ac8828d2deb3b45dd38f2b9204ca73e908a6702a
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:53:43 +0300

    Update host.py

--- host.py
+++ host.py
@@ -1,9 +1,20 @@
+import telebot
+import time
+import random
 import threading
+from telebot import types
+from pymongo import MongoClient
+import traceback
 
 def hostloshod(test):
+    token = '587282753:AAHHyjXezDjRy7osT_JmR9raPay0CwbXRRY'
+    bot = telebot.TeleBot(token)
     print('Launching "loshadkin.py"...')
     exec(open("loshadkin.py").read())
+    
 def hostgbball(test):
+    token = '765563788:AAFnkelmaKJkalYOAJsU0fTrexErKOnl5No'
+    bot = telebot.TeleBot(token)
     print('Launching "gbball.py"...')
     exec(open("gbball.py").read())    
 threading.Timer(1,hostloshod,args=['test']).start()
