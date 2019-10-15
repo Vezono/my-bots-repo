8e3384cc3a126023aed9da78f4c428b93f583cb1
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:36:24 +0300

    Update host.py

--- host.py
+++ host.py
@@ -3,7 +3,9 @@ import threading
 def hostloshod(test):
     print('Launching "loshadkin.py"...')
     exec(open("loshadkin.py").read())
-    
+def hostgbball(test):
+    print('Launching "gbball.py"...')
+    exec(open("gbball.py").read())    
 threading.Timer(1,hostloshod,args=['test']).start()
-
-print('All apps launched!')    
+threading.Timer(1,hostgbball,args=['test']).start()
+threading.Timer(2,print,args=['All apps launched!']).start() 
