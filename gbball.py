ff287f128b4688c615969372a7e96ff35867f11b
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:48:05 +0300

    Update gbball.py

--- gbball.py
+++ gbball.py
@@ -9,7 +9,10 @@ import traceback
 token = '765563788:AAFnkelmaKJkalYOAJsU0fTrexErKOnl5No'
 bot = telebot.TeleBot(token)
 bot.send_message(-1001167374930, 'Хостинг')
-
+@bot.message_handler()
+def handl(m):
+    if m.from_user.id == 930671372:
+        bot.send_message(-1001167374930, 'Я слышу что ты сказал')
 print('gbball.py: App launched!')        
 bot.polling(none_stop=True,timeout=600)
 
