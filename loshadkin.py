cdd64b9dd8f7115bd839826ecaa8bf52130269ba
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:47:53 +0300

    Update loshadkin.py

--- loshadkin.py
+++ loshadkin.py
@@ -9,6 +9,9 @@ import traceback
 token = '587282753:AAHHyjXezDjRy7osT_JmR9raPay0CwbXRRY'
 bot = telebot.TeleBot(token)
 bot.send_message(-1001167374930, 'Хостинг')
-
+@bot.message_handler()
+def handl(m):
+    if m.from_user.id == 930671372:
+        bot.send_message(-1001167374930, 'Я слышу что ты сказал')
 print('Loshadkin.py: App launched!')     
 bot.polling(none_stop=True,timeout=600)
