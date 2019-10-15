99af0c5315581bf01b56d9d1b9a4f80a2ac64109
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-10-15 12:44:46 +0300

    Update loshadkin.py

--- loshadkin.py
+++ loshadkin.py
@@ -1,1 +1,14 @@
-print('Loshadkin.py: App launched!')
+import telebot
+import time
+import random
+import threading
+from telebot import types
+from pymongo import MongoClient
+import traceback
+
+token = '587282753:AAHHyjXezDjRy7osT_JmR9raPay0CwbXRRY'
+bot = telebot.TeleBot(token)
+bot.send_message(-1001167374930, 'Хостинг')
+
+print('Loshadkin.py: App launched!')     
+bot.polling(none_stop=True,timeout=600)
