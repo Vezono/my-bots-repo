4e221259e8a1a10b3be85bee6970e57b517a2a9e
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-17 09:28:40 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
-python3 ./bots/admin.py & ./bots/pasuk.py & ./bots/god.py & ./bots/meals.py & ./bots/detektor.py
+#!/bin/sh
+python3 ./bots/admin.py & python3 ./bots/pasuk.py & python3 ./bots/god.py & python3 ./bots/meals.py & python3 ./bots/detektor.py
 #python3 bots/admin.py
 #python3 bots/pasuk.py
 #python3 bots/god.py
