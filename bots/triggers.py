6b0bd84458f4aa6b1775f62effab93776e949a5e
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-17 12:41:34 +0200

    Update triggers.py

--- bots/triggers.py
+++ bots/triggers.py
@@ -33,9 +33,7 @@ def gettriggers(m):
     tts = 'Ваши триггеры:\n'
     t = 0
     for trigger in chat_triggers.keys():
-        if t > 3:
-            tts += '{}|{}\n'.format(trigger, chat_triggers)
-        t += 1
+        tts += '{}|{}\n'.format(trigger, chat_triggers)
     bot.reply_to(m, tts)
 @bot.message_handler(commands=['addtrigger'])
 def addtrigger(m):
