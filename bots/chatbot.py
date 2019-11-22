0cc8b32c7056493681e320b54861d5372096ae49
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-22 20:56:39 +0200

    Update chatbot.py

--- bots/chatbot.py
+++ bots/chatbot.py
@@ -14,7 +14,7 @@ col = db.chatting
 #print(col.find_one({'group_id':{'$exists':True}})['msgs_to_index'])
 
 def write_to_db(m):
-	if m.reply_to_message and m.text != None and m.reply_to_message.text != None and m.reply_to_message.from_user.is_bot != True and not m.reply_to_message.text.startswith('@') and not m.reply_to_message.text.startswith('/'):
+	if m.reply_to_message and m.text and m.reply_to_message.text and not m.reply_to_message.from_user.is_bot and not m.reply_to_message.text.startswith('@') and not m.reply_to_message.text.startswith('/'):
 		col.update_one({'group_id':m.chat.id},
 				{'$push':{'msgs_to_index': m.reply_to_message.text.lower()}},
 				upsert=True)
@@ -27,8 +27,8 @@ def chatting(m):
 	try:
 		if col.find_one({'group_id':m.chat.id}):
 			msgs_to_index = col.find_one({'group_id':m.chat.id})['msgs_to_index']
-			if random.randint(1, 3) % 3 == 0:
-				if m.text.lower() in msgs_to_index and m.reply_to_message == None or m.text in msgs_to_index and m.reply_to_message.from_user.is_bot != True:
+			if True:
+				if m.text.lower() in msgs_to_index and not m.reply_to_message or m.text in msgs_to_index and not m.reply_to_message.from_user.is_bot:
 					answer = col.find_one({'group_id':m.chat.id})[m.text.lower().replace('.', 'dot_replace')]
 					answer = random.choice(answer)
 					bot.send_message(m.chat.id, answer, reply_to_message_id = m.message_id)				
