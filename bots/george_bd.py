31b758bf927af0a27605b97f383f43070716053a
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-22 21:28:47 +0200

    Update george_bd.py

--- bots/george_bd.py
+++ bots/george_bd.py
@@ -12,11 +12,23 @@ client = pymongo.MongoClient(os.environ['database2'])
 db = client.test
 bottle = db.bottle
 privates = db.privates
+collection = client.bot_father.pin_list
 def getjrinfo(m):
     if m.text.count(' '):
         group = int(m.text.split(' ')[1])
         active_players = [doc['boyar'], doc['jester'], doc['king']]
         return str(active_players)
+def sethelp(help):
+    collection.update_one({'id': 0},
+                              {'$set': {'help_msg': help}},
+                              upsert=True)
+    bot.send_message(admin, 'completed.')
+
+@bot.message_handler(commands=['sethelp'])
+def shelp(m):
+    if m.from_user.id == admin:
+        sethelp(m.text.split(' ', 1)[1])
+        
 print('Gog works!')
 runner = BotsRunner([admin])
 runner.add_bot('Goggy', bot)
