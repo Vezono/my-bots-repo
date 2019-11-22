4528295a168eef01af67ba06e99eda6536c3de8e
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-22 21:10:56 +0200

    Update george_bd.py

--- bots/george_bd.py
+++ bots/george_bd.py
@@ -9,7 +9,14 @@ admin = 792414733
 
 bot = telebot.TeleBot(os.environ['gogbot'])
 client = pymongo.MongoClient(os.environ['database2'])
-
+db = client.test
+bottle = db.bottle
+privates = db.privates
+def getjrinfo(m):
+    if m.text.count(' '):
+        group = int(m.text.split(' ')[1])
+        active_players = [doc['boyar'], doc['jester'], doc['king']]
+        return str(active_players)
 print('Gog works!')
 runner = BotsRunner([admin])
 runner.add_bot('Goggy', bot)
