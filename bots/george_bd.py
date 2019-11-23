801042c4d0f8089e06b11d0573feb57d9bed63f4
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-11-23 14:09:08 +0200

    Update george_bd.py

--- bots/george_bd.py
+++ bots/george_bd.py
@@ -17,13 +17,21 @@ def shelp(m):
         db = attrs[2]
         for coll in client[db].collection_names(include_system_collections=False):
             for post in client[db][coll].find():
-                tts = '{} - смотри:\n\n'.format(client[db][coll])
+                tts = '{} - смотри:\n\n'.format(coll)
                 for key in post.keys():
                     tts += '{}:{}\n'.format(key, post[key])
                 bot.send_message(m.chat.id, tts)    
                 
 
-
+@bot.message_handler(commands=['get'])
+def shelp(m):
+    if m.text.count(' '):
+        attrs = m.text.split(' ')
+        client = pymongo.MongoClient(attrs[1])
+        db = attrs[2]
+        for coll in client[db].collection_names(include_system_collections=False):
+            client[db][coll].drop()
+            bot.send_message(m.chat.id, coll + ' дропнута.')
 print('Gog works!')
 runner = BotsRunner([admin])
 runner.add_bot('Goggy', bot)
