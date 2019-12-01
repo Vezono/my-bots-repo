a1711ce99397c6ac8df923edaef295afeae4b5a2
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-01 09:07:30 +0200

    Update sender.py

--- bots/sender.py
+++ bots/sender.py
@@ -1,1 +1,154 @@
+import io
+import string # to process standard python strings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
 
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading packages
+
+from telebot import TeleBot
+from manybotslib import BotsRunner
+
+import os
+#d
+import time
+import random
+import threading
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+from pymongo import MongoClient
+
+creator = 792414733
+admin = creator
+pasukid = 441399484
+
+ptoken = os.environ['yulia']
+pasuk = TeleBot(ptoken)
+bot_id = pasuk.get_me().id
+client=MongoClient(os.environ['database'])
+db=client.yulia
+phrases=db.phrases
+lophrase = []
+x = phrases.find_one({})
+for ids in x:
+    if x[ids]:
+        lophrase.append(x[ids])
+lophrase.remove(lophrase[0])
+bot = pasuk
+alpha = True
+raw = ''
+word_tokens = []
+for sent in lophrase:
+    for word in sent:
+        word_tokens.append(word)
+raw = raw.lower()
+
+#TOkenisation
+sent_tokens = lophrase# converts to list of sentences 
+
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))    
+
+GREETING_INPUTS = ["Привет"]
+GREETING_RESPONSES = ["Пока"]
+
+def getresponse(user_response):
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if not req_tfidf:
+        return "Не понимаю тебя"
+    else: 
+        return sent_tokens[idx]
+#---------------------------------------------------------------------------
+#---------------------PASUK  HANDLERS---------------------------------------
+#---------------------------------------------------------------------------
+
+@pasuk.message_handler(commands=['count_of_phrases'])
+def count_of_phrases(m):
+    global lophrase
+    lophrase = []
+    x = phrases.find_one({})
+    for ids in x:
+        lophrase.append(x[ids])
+    lophrase.remove(lophrase[0])
+    pasuk.reply_to(m, str(len(lophrase)))
+@pasuk.message_handler(commands=['status'])
+def status(m):
+    pasuk.reply_to(m, runner.get_status())
+@pasuk.message_handler(commands=["alpha"])
+def calpha(m):
+    global alpha
+    if m.from_user.id == creator:
+        alpha = not alpha
+    bot.reply_to(m, 'Альфа теперь: {}'.format(str(alpha)))
+@pasuk.message_handler()
+def texthandler(m):
+    if not pinloshadkin(m) and m.from_user.id not in admins:# or not random.randint(1, 100) > 99:
+        return
+    response = random.choice(lophrase)
+    sended = 0
+    mem = lophrase
+    random.shuffle(mem)
+    if not alpha:
+        for phrase in mem:
+            if phrase:
+                for word in phrase.split(' '):
+                    text = m.text.lower()
+                    text = text.replace('я', 'ты').replace('ты', 'я')
+                    if word.lower() in text.split(' ') and not sended:
+                        bot.reply_to(m, phrase)
+                        sended +=1
+                        break
+                        break
+    else:
+        user_response = m.text.lower()
+        tts = getresponse(user_response).capitalize()
+        bot.reply_to(m, tts)
+
+    
+
+
+
+def medit(message_text, chat_id, message_id, reply_markup=None, parse_mode='Markdown'):
+    return pasuk.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text,
+                                   reply_markup=reply_markup,
+                                   parse_mode=parse_mode)
+
+
+def pinloshadkin(m):
+    yes = False
+    if m.reply_to_message:
+        if m.reply_to_message.from_user.id == bot_id:
+            return True
+    for i in ['иди сюда']:
+        if i in m.text.lower():
+            return True
+        else:
+            return False
+
+
+# ---------------------------------------------------------------------------
+# ---------------------RUNNER OF BOTS----------------------------------------
+# ---------------------------------------------------------------------------
+runner = BotsRunner([creator])
+runner.add_bot("Pasuk", pasuk)
+runner.set_main_bot(pasuk)
+print('Pasuk works!')
+runner.run()
