9b19b31e5267b6ebdcb6b7b78e13b6a1c47fb99b
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-01 22:07:15 +0200

    Update george_bd.py

--- bots/george_bd.py
+++ bots/george_bd.py
@@ -11,9 +11,14 @@ bot = telebot.TeleBot(os.environ['gogbot'])
 def get_file(file, col):
     if not col.find_one({file: {'$exists': True}}):
         return
-    with open('file', 'wb') as f:
+    with open(file, 'wb') as f:
         f.write(col.find_one({'_id': {'$exists': True}})[file])
     return file    
+def write_file(text):
+    with open('file', 'wb') as f:
+        f.write(tts)
+    return file
+
 @bot.message_handler(commands=['get'])
 def sget(m):
     if m.text.count(' '):
@@ -46,12 +51,25 @@ def sfile(m):
         coll = client[db][attrs[3]]
         file = attrs[4]
         if get_file(file, coll):
-            bot.send_file(m.chat.id, get_file(file, coll))
+            bot.send_document(m.chat.id, get_file(file, coll))
         else:
             bot.reply_to(m, 'Такого файла нет!')
+@bot.message_handler(commands=['get_as_file'])
+def sgetasfile(m):            
+    if m.text.count(' '):
+        attrs = m.text.split(' ')
+        client = pymongo.MongoClient(attrs[1])
+        db = attrs[2]
+        for coll in client[db].collection_names(include_system_collections=False):
+            for post in client[db][coll].find():
+                tts = '{} - смотри:\n\n'.format(coll)
+                for key in post.keys():
+                    tts += '{}:{}\n'.format(key, post[key])
+                bot.send_document(m.chat.id, file_write(tts))   
+
 @bot.message_handler(commands=['help'])
 def shelp(m): 
-    bot.reply_to(m, '/get <ссылка на клиент> <имя датабазы> - выводит все данные в датабазе.\n/drop <ссылка на клиент> <имя датабазы> - удаляет все данные в коллекции.\n/file <ссылка на клиент> <имя датабазы> <имя коллекции> <имя файла>')
+    bot.reply_to(m, '/get <ссылка на клиент> <имя датабазы> - выводит все данные в датабазе.\n/get_as_file - работает так же как и /get, но возвращает все ввиде файлов\n/drop <ссылка на клиент> <имя датабазы> - удаляет все данные в коллекции.\n/file <ссылка на клиент> <имя датабазы> <имя коллекции> <имя файла>')
 print('Gog works!')
 runner = BotsRunner([admin])
 runner.add_bot('Goggy', bot)
