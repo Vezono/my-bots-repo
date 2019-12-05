7ef625cba810f97e953fc241e2232bdcf304f921
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-05 22:00:13 +0200

    Update antosha.py

--- bots/antosha.py
+++ bots/antosha.py
@@ -13,6 +13,10 @@ import traceback
 bot_token = os.environ['antosha']
 bot = telebot.TeleBot(bot_token)
 
+client=MongoClient(os.environ['database'])
+db=client.antosha
+phrases=db.phrases
+
 creator = 792414733
 
 timings = '''
@@ -45,6 +49,50 @@ def joke(m):
 @bot.message_handler(commands=['addjoke'])
 def addjoke(m):
     pass
+@bot.message_handler(commands=['tea'])
+def ftea(m):
+    if m.reply_to_message:
+        from_user = m.from_user.first_name
+        if m.text.count(' ') == 0:
+            tea = 'обычный'
+        else:
+            tea = m.text.split(' ', 1)[1]
+        touser = m.reply_to_message.from_user.first_name
+        ahref = '<a href="tg://user?id={}">{}</a>'.format(m.reply_to_message.from_user.id, touser)
+        kb = types.InlineKeyboardMarkup()
+        btns = []
+        btns2 = []
+        for i in ['drink', 'reject']:
+            btns.append(types.InlineKeyboardButton(rus(i), callback_data='{} {}'.format(i, touser)))
+        for i in ['throw']:
+            btns2.append(types.InlineKeyboardButton(rus(i), callback_data='{} {}'.format(i, touser)))
+        kb.add(*btns)
+        kb.add(*btns2)
+        if touser == bot.get_me().first_name:
+            tts = 'Н-но это я должен готовить чай вам, ' + from_user + '!!'
+            kb = None
+        else:
+            tts = '{} приготовил чай "{}" для вас, {}!'.format(from_user, tea, ahref)
+        bot.delete_message(m.chat.id, m.message_id)
+        bot.send_message(m.chat.id, tts, reply_markup=kb, parse_mode='HTML')
+    else:
+        if m.text.count(' ') == 0:
+            tea = 'обычный'
+        else:
+            tea = m.text.split(' ', 1)[1]
+        tts = '{} заварил себе чай "{}"!'.format(m.from_user.first_name, tea)
+        bot.delete_message(m.chat.id, m.message_id)
+        bot.send_message(m.chat.id, tts)
+
+r = {'drink': 'Выпить',
+     'reject': 'Отказаться',
+     'throw': 'Вылить'}
+
+def rus(name):
+    try:
+        return r[name]
+    except:
+        return name
 runner = BotsRunner([792414733]) # pass empty list if you don't want to receive error messages on fail
 runner.add_bot("Antosha", bot)
 runner.set_main_bot(bot)
