0a8e63b6cf1b88aa3112e28b806c795cebaffae7
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-05 21:32:06 +0200

    Update antosha.py

--- bots/antosha.py
+++ bots/antosha.py
@@ -13,12 +13,15 @@ import traceback
 bot_token = os.environ['antosha']
 bot = telebot.TeleBot(bot_token)
 
+creator = 792414733
+
 @bot.message_handler(commands=['status'])
 def status(m):
     bot.reply_to(m, runner.get_status())
 @bot.message_handler(commands=['getm'])
 def status(m):
-    bot.reply_to(m, str(m))
+    if m.from_user.id == creator:
+        bot.reply_to(m, str(m))
 runner = BotsRunner([792414733]) # pass empty list if you don't want to receive error messages on fail
 runner.add_bot("Antosha", bot)
 runner.set_main_bot(bot)
