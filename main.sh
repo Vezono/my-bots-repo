d3a4bb6ada4370783fadc596aff96c4291a4cae6
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-05 21:24:52 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 ./bots/admin.py & python3 ./bots/lazertag.py & python3 ./bots/pasuk.py & python3 ./bots/lifegame.py & python3 ./bots/minecraft.py & python3 ./bots/triggers.py & python3 ./bots/meals.py & python3 ./bots/detektor.py & python3 ./bots/hawkeye.py & python3 ./bots/aiwordgen.py & python3 ./bots/sender.py & python3 ./bots/george_bd.py
+python3 ./bots/admin.py & python3 ./bots/lazertag.py & python3 ./bots/antosha.py & python3 ./bots/pasuk.py & python3 ./bots/lifegame.py & python3 ./bots/minecraft.py & python3 ./bots/triggers.py & python3 ./bots/meals.py & python3 ./bots/detektor.py & python3 ./bots/hawkeye.py & python3 ./bots/aiwordgen.py & python3 ./bots/sender.py & python3 ./bots/george_bd.py
 #cd $(dirname $0)
 #python3 /app/bots/controller.py
