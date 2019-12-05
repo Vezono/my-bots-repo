5393178dfb1cd8648dcba0bee9c6fe1c03f5cc5e
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-05 21:50:15 +0200

    Update antosha.py

--- bots/antosha.py
+++ bots/antosha.py
@@ -25,7 +25,10 @@ timings = '''
 
 ДЕДЛАЙН РЕЛИЗА ФАНФИКА - 3 ЯНВАРЯ.
 '''
-
+@pasuk.message_handler()
+def texthandler(m):
+    if 'бот' in m.text.lower() or 'антон' in m.text.lower():
+        bot.reply_to(m, random.choice(['Бляха', 'I`ll be back']))
 @bot.message_handler(commands=['status'])
 def status(m):
     bot.reply_to(m, runner.get_status())
