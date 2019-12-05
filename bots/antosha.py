8e72e477f6e671fb5fb9b24a906394a992158755
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-05 21:27:04 +0200

    Update antosha.py

--- bots/antosha.py
+++ bots/antosha.py
@@ -16,9 +16,11 @@ bot = telebot.TeleBot(bot_token)
 @bot.message_handler(commands=['status'])
 def status(m):
     bot.reply_to(m, runner.get_status())
-
+@bot.message_handler(commands=['getm'])
+def status(m):
+    bot.reply_to(m, str(m.reply_to_message))
 runner = BotsRunner([792414733]) # pass empty list if you don't want to receive error messages on fail
-runner.add_bot("Book", bot)
+runner.add_bot("Antosha", bot)
 runner.set_main_bot(bot)
-print('Book')
+print('Antosha works!')
 runner.run()
