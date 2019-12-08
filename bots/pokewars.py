ec7dd61f09c4c5db3ddaec8ee5b3173796c43fa9
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-08 19:01:12 +0200

    Update pokewars.py

--- bots/pokewars.py
+++ bots/pokewars.py
@@ -104,7 +104,6 @@ def statssss(m):
 def tatar(m):
     if not tatar and m.from_user.id not in vip:
         bot.reply_to(m, 'Вы недавно уже призывали монгол.')
-        global tatar
         tatar = False
         return
     bot.reply_to(m, 'МОНГОЛЫ ПРИНИМАЮТ ВАШ ВЫЗОВ.')
