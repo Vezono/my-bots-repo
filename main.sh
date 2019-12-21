ac4e465d416af1645ed346c2f4553c9a4ee21c91
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-21 19:10:40 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 ./bots/lazertag.py & python3 ./bots/pasuk.py & python3 ./bots/triggers.py & python3 ./bots/aiwordgen.py & python3 ./bots/britbot.py & python3 ./bots/sender.py & python3 ./bots/randomer.py #& python3 ./bots/pokewars.py & python3 ./bots/george_bd.py
+python3 ./bots/lazertag.py & python3 ./bots/triggers.py & python3 ./bots/aiwordgen.py & python3 ./bots/britbot.py & python3 ./bots/sender.py & python3 ./bots/randomer.py #& python3 ./bots/pokewars.py & python3 ./bots/george_bd.py & python3 ./bots/pasuk.py 
