03cd23faae470744702746fbe67b135e3ff67b37
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-27 15:22:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import os
 
 # environ = os.environ
-environ = {'database':'mongodb://gbball:onionland@database-shard-00-00-fcfmt.gcp.mongodb.net:27017,'
-                      'database-shard-00-01-fcfmt.gcp.mongodb.net:27017,'
-                      'database-shard-00-02-fcfmt.gcp.mongodb.net:27017/test?ssl=true&replicaSet=database-shard-0'
-                      '&authSource=admin&retryWrites=true&w=majority',
-           'britbot':'970751955:AAEFdmjC8_nblWSPiVyxnGTRH3nRUGgACUw',
-           'gogbot':'1017113506:AAFPTkBxF-VFhbVEyAwZ-J2C8kHMDh4IshE'}
+
 
 creator = 792414733
 
