f74d8bebb44e3424393bc72522cad480408b0b03
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-01 22:20:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from bots import britbot
+from bots import sender
+from bots import randomer
+from bots import george_bd
+from bots import pasuk
+
+from modules.manybotslib import BotsRunner
+
+from modules.funcs import BotUtil
+
