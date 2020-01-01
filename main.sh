d316fb38cb8490d4582c4ab81a1f0cce65bb495e
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-01 21:22:48 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+var1 = 5
+while [ $var1 -gt 0 ]
+do
 python3 ./bots/britbot.py & python3 ./bots/sender.py & python3 ./bots/randomer.py & python3 ./bots/george_bd.py & python3 ./bots/pasuk.py 
+done
