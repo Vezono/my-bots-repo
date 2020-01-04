068494662100260bf88875fba81fd01da1f8793b
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-04 15:37:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ from bots import randomer
 from bots import george_bd
 from bots import pasuk
 
+import config
+
 from modules.manybotslib import BotsRunner
 
 from modules.funcs import BotUtil
 
+bot = BotUtil('1006055451:AAGta5Mx9nIH6CTal0pirJp-n3wvsq2QKRk')
+
+bots = {
+    'Брит': britbot.bot,
+    'НейроЮля': sender.bot,
+    'Mr. Random': randomer.bot,
+    'DB Checker': george_bd.bot,
+    'Пасюк': pasuk.bot,
+    'Bot Ruler': bot
+}
+
+runner = BotsRunner(config.creator, 0, True)
+runner.add_bots(bots)
+runner.set_main_bot(bot, 'status')
+
+runner.run()
