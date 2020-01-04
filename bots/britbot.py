908de683d71bd9f55a12214e96c633ed4451899b
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-04 15:37:50 +0200

    Update britbot.py

--- bots/britbot.py
+++ bots/britbot.py
@@ -238,10 +238,3 @@ def rus(name):
         return r[name]
     except:
         return name
-
-
-runner = BotsRunner([creator])  # pass empty list if you don't want to receive error messages on fail
-runner.add_bot("Brit", bot)
-runner.set_main_bot(bot)
-print('Brit works!')
-runner.run()
