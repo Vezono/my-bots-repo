929ac244f8e66bfbdca5f9c0748a61947bebca6c
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-04 15:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ bots = {
     'Bot Ruler': bot
 }
 
-runner = BotsRunner((config.creator), 0, True)
+runner = BotsRunner(admins=(config.creator), retries=3, show_traceback=True)
 runner.add_bots(bots)
 runner.set_main_bot(bot, 'status')
 
