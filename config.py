bfdba0aa2c6b0e0d7b2156716a69b0dc4a86c483
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-09 15:36:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
 import os
 
 environ = os.environ
-environ = {'database': 'mongodb://gbball:onionland@database-shard-00-00-fcfmt.gcp.mongodb.net:27017,'
-                       'database-shard-00-01-fcfmt.gcp.mongodb.net:27017,'
-                       'database-shard-00-02-fcfmt.gcp.mongodb.net:27017/test?ssl=true&replicaSet=database-shard-0'
-                       '&authSource=admin&retryWrites=true&w=majority',
-           'pasuk': '976911963:AAFzMGQBIlKruwL9HUVzy2Bsc4XjW0bYK2I',
-           'yuliaid': '94197300',
-           'randomer': '947351700:AAHw0uwA8C-urWPtwIFrGWMxzd6VdGhe27E',
-           'gogbot': '1017113506:AAFPTkBxF-VFhbVEyAwZ-J2C8kHMDh4IshE',
-           'britbot': '970751955:AAEFdmjC8_nblWSPiVyxnGTRH3nRUGgACUw',
-           'yulia': '847114026:AAEirGBGcknUh9UA70xTQxzPG39leSCMxYs'}
 
 creator = 792414733
 
