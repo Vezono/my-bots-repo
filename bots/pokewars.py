fcb531b8dc67325915aad11f28e1b9775c018fb6
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-01-09 16:35:06 +0200

    Update pokewars.py

--- bots/pokewars.py
+++ bots/pokewars.py
@@ -48,8 +48,8 @@ symbollist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '
               'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
               'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
               '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-pokemonlist = list(pokemons.keys())
+pokemons = {}
+pokemonlist = []
 def medit(message_text, chat_id, message_id, reply_markup=None, parse_mode='Markdown'):
     return bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text, reply_markup=reply_markup,
                                  parse_mode=parse_mode)
