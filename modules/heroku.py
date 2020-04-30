aac64ac4672acb9f53c83b0a60e341cbd87de92c
vezono <vezonolab@gmail.com>
2020-04-30 18:18:18 +0300

    remove polling

--- modules/heroku.py
+++ modules/heroku.py
@@ -1,5 +1,3 @@
-import time
-
 import heroku3
 
 import config
@@ -10,11 +8,4 @@ class Heroku():
         self.bot = bot
         self.app = heroku3.from_key(config.environ['heroku_key']).apps()['gbball-great-host']
         self.last_log = ''
-        self.polling()
 
-    def polling(self):
-        time.sleep(1)
-        if self.app.get_log(lines=1) == self.last_log:
-            self.polling()
-        self.last_log = self.app.get_log(lines=1)
-        self.bot.report(self.last_log, quiet=True)
