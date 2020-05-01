4d8b79bc005645fc0ba7e46915f54a33daca0c0a
vezono <vezonolab@gmail.com>
2020-05-01 12:40:24 +0300

    autoimports

--- bots/georges_db.py
+++ bots/georges_db.py
@@ -11,20 +11,6 @@ admin = 792414733
 bot = telebot(config.environ['georges_db'])
 
 
-def get_file(file, col):
-    if not col.find_one({file: {'$exists': True}}):
-        return
-    with open(file, 'wt') as f:
-        f.write(str(col.find_one({'_id': {'$exists': True}})[file]))
-    return f.open()
-
-
-def write_file(text):
-    with open('file', 'wt') as f:
-        f.write(text)
-    return f.open()
-
-
 @bot.message_handler(commands=['get'])
 def sget(m):
     if m.text.count(' '):
@@ -50,41 +36,15 @@ def sdrop(m):
             bot.send_message(m.chat.id, coll + ' дропнута.')
 
 
-@bot.message_handler(commands=['file'])
-def sfile(m):
-    if m.text.count(' '):
-        attrs = m.text.split(' ')
-        client = pymongo.MongoClient(attrs[1])
-        db = attrs[2]
-        coll = client[db][attrs[3]]
-        file = attrs[4]
-        if get_file(file, coll):
-            bot.send_document(m.chat.id, data=get_file(file, coll))
-        else:
-            bot.reply_to(m, 'Такого файла нет!')
 
-
-@bot.message_handler(commands=['get_as_file'])
-def sgetasfile(m):
-    if m.text.count(' '):
-        attrs = m.text.split(' ')
-        client = pymongo.MongoClient(attrs[1])
-        db = attrs[2]
-        for coll in client[db].collection_names(include_system_collections=False):
-            for post in client[db][coll].find():
-                tts = '{} - смотри:\n\n'.format(coll)
-                for key in post.keys():
-                    tts += '{}:{}\n'.format(key, post[key])
-                bot.send_document(m.chat.id, data=write_file(tts))
 
 
 @bot.message_handler(commands=['help'])
 def shelp(m):
     bot.reply_to(m,
-                 '/get <ссылка на клиент> <имя датабазы> - выводит все данные в датабазе.\n/get_as_file - работает '
-                 'так же как и /get, но возвращает все ввиде файлов\n/drop <ссылка на клиент> <имя датабазы> - '
-                 'удаляет все данные в коллекции.\n/file <ссылка на клиент> <имя датабазы> <имя коллекции> <имя '
-                 'файла>')
+                 '/get <ссылка на клиент> <имя датабазы> - выводит все данные в датабазе.\n'
+                 '/drop <ссылка на клиент> <имя датабазы> - '
+                 'удаляет все данные в коллекции.\n')
 
 
 @bot.message_handler(commands=['api'])
--- modules/heroku.py
+++ modules/heroku.py
@@ -8,4 +8,3 @@ class Heroku():
         self.bot = bot
         self.app = heroku3.from_key(config.environ['heroku_key']).apps()['gbball-great-host']
         self.last_log = ''
-
