c5d941984af48f7164a63892b57f66b0077a72b7
vezono <vezonolab@gmail.com>
2020-06-03 19:39:00 +0300

    fix startfile

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sh bots/unobot/locales/compile.sg
-nohup python3 main.py & python3 bots/mau_mau_bot/bot.py
+sh bots/unobot/locales/compile.sh
+nohup python3 main.py & python3 ./bots/mau_mau_bot/bot.py
