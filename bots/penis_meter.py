94bdafc5445b30b42c1a344628ae75fecb70dd90
vezono <vezonolab@gmail.com>
2020-06-06 22:46:27 +0300

    fix renaming tg user and db user

--- bots/penis_meter.py
+++ bots/penis_meter.py
@@ -35,7 +35,7 @@ def top_handler(m):
     top.reverse()
     for index in range(len(top)):
         user = top[index]
-        tts += f'{index}. {user["penis_name"]} ({user["name"]}): {user["length"]}'
+        tts += f'{index + 1}. {user["penis_name"]} ({user["name"]}): {user["length"]} см'
     bot.reply_to(m, tts)
 
 
@@ -54,6 +54,21 @@ def give_handler(m):
     user = get_user(m.from_user.id)
     users.update_one(user, {'$inc': {'length': growth}})
     bot.reply_to(m, f'Вы изменили себе пипирку на {growth}.')
+
+
+@bot.message_handler(commands=['name'])
+def name_handler(m):
+    if not m.text.count(' '):
+        return
+    name = m.text.split(' ')[1]
+    if m.reply_to_message:
+        user = get_user(m.reply_to_message.from_user)
+        users.update_one(user, {'$set': {'penis_name': name}})
+        bot.reply_to(m.reply_to_message, f'Вам изменили имя пипирки на {name}.')
+        return
+    user = get_user(m.from_user.id)
+    users.update_one(user, {'$set': {'penis_name': name}})
+    bot.reply_to(m, f'Вы изменили себе имя пипирки на {name}.')
 
 
 @bot.message_handler(commands=['cut'])
