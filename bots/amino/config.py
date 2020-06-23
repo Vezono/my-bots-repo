e3cd0b7b670ea140ff324d36c8c8afd422161ce9
vezono <vezonolab@gmail.com>
2020-06-23 20:31:33 +0300

    add amino bot

--- bots/amino/config.py
+++ bots/amino/config.py
@@ -1,9 +1,16 @@
+try:
+    from tokens import environ
+except ImportError:
+    import os
+
+    environ = os.environ
+
 com_id = '158702123'
 cn_id = '0bc640b0-5b4f-4ec9-a269-35b03e883ec3'
 email = 'gbball.trash@gmail.com'
-password = 'тттттт'
-mongo_token = 'mongodb+srv://vezono:coMMunism1991@database-fcfmt.gcp.mongodb.net/test?retryWrites=true&w=majority'
+password = environ['amino_password']
+mongo_token = environ['database']
 brit_id = '6e347e8a-dda4-4cb9-8e0d-01e4062df351'
-t_token = '1084180557:AAHuspLkypy751MvQ4CqURIir_nNIS8FZc8'
+t_token = environ['assistant']
 tg_cn_id = -1001333052188
 tg_brit_id = 792414733
